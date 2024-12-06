5695f3667a7f49262e1c6be189946428546219f9
pensando <benjamin.maze@gmail.com>
2024-12-06 13:09:26 +0000

    add compose for macvlan and add dns

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+//parameter for php script
+//$ip_mysql = '10.29.21.29';
+//$ip_mqttbroker = '10.29.21.29';
+$ip_mysql = '10.29.21.31';
+$ip_mqttbroker = '10.29.21.30';
+
+$ip_psm = '10.9.20.71';
+$ip_backend = '10.29.21.34';
+$user_mysql = 'mqtt';
+$pwd_mysql='mqtt';
+$usr_mqtt = 'mqtt';
+$pwd_mqtt = 'mqtt';
+$usr_PSM = 'admin';
+$pwd_PSM = 'Pensando0$';
+$policy_PSM = "pod1-vrf-auto";
+?>
---
+++ config/config.py
@@ -0,0 +1,11 @@
+#parameter for mysql-publish.py & mqtt-publish.py
+#ip_mysql = '10.29.21.31'
+#ip_mqttbroker = '10.29.21.30'
+ip_mysql = 'db.pod1'
+ip_mqttbroker = 'mosquitto.pod1'
+
+user_mysql = 'mqtt'
+pwd_mysql='mqtt'
+usr_mqtt = 'mqtt'
+pwd_mqtt = 'mqtt'
+ratio_session = 10
