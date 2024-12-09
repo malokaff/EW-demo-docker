42d3a92b2789ec00e661d4275f84468cc69896cf
pensando <benjamin.maze@gmail.com>
2024-12-09 15:03:46 +0000

    add script to automate initial config (docker-compose-prep.yml)

---
+++ config/template/config-template.php
@@ -0,0 +1,17 @@
+<?php
+//parameter for php script
+//$ip_mysql = '10.29.21.29';
+//$ip_mqttbroker = '10.29.21.29';
+$ip_mysql = '$IpMysql';
+$ip_mqttbroker = '$IpMosquitto';
+
+$ip_psm = '$IpPSM';
+$ip_backend = '$IpBackend';
+$user_mysql = 'mqtt';
+$pwd_mysql='mqtt';
+$usr_mqtt = 'mqtt';
+$pwd_mqtt = 'mqtt';
+$usr_PSM = '$UserPSM';
+$pwd_PSM = '$PasswordPSM';
+$policy_PSM = "$PolicyApiPSM";
+?>
