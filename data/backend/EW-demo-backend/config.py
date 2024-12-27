9a7d5b8b16d2d381c6fe72a21b23ca391fb686ec
pensando <benjamin.maze@gmail.com>
2024-12-27 08:55:48 +0000

    update README

--- data/backend/EW-demo-backend/config.py
+++
@@ -1,8 +0,0 @@
-#parameter for mysql-publish.py & mqtt-publish.py
-ip_mysql = '192.168.69.28'
-ip_mqttbroker = '192.168.69.28'
-user_mysql = 'mqtt'
-pwd_mysql='mqtt'
-usr_mqtt = 'mqtt'
-pwd_mqtt = 'mqtt'
-ratio_session = 10
